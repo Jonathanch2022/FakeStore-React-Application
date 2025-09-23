ad04b9c40c728632bbcbe42cb1a38a1fb5657638
Jonathanch2022 <filestreamers@gmail.com>
2025-09-23 00:54:27 -0400

    Edited Profile order history style

--- src/components/CollapasableContainer.jsx
+++ src/components/CollapasableContainer.jsx
@@ -3,37 +3,37 @@ import "../css/collapseContainer.css"
 import CartItem from "./CartItem"
 export default function CollapsContainer(props) {
 
+   
 
+    return (
+        <>
+       
 
-    return (<>
-
-        <div className="collapseDiv">
+                <div id="h1container">
+                    <div id="header">Date: {props.date}</div>
+                    <div id="header1">ID: {props.id}</div>
+                </div>
+                <div className="collapseDiv">
            
-            <div id="h1container">
-                <div id="header">Date: {props.date}</div>
-                <div id="header1">ID: {props.id}</div>
-            </div>
-            <div className="collapseHeading">
-                {
-                    props.history.map((item) => {
+         
+                    <div  className="collapseHeading">
+                        {
+                            props.history.map((item) => {
 
                         
-                        return (
-                            <CartItem id={item.id} quantity={item.quantity} image={item.image} title={item.title} price={item.price} cantEdit={true} ></CartItem>
-                        )
-                    })
-                }
+                                return (
+                                    <CartItem id={item.id} quantity={item.quantity} image={item.image} title={item.title} price={item.price} cantEdit={true} ></CartItem>
+                                )
+                            })
+                        }
 
+                    </div>
+                    <div id="h1containerbt">
+                        <div id="headerbt">Total: {props.total}</div>
+                    </div>
             </div>
-            <div id="h1containerbt">
-                <div id="headerbt">Total: {props.total}</div>
-            </div>
-            
-            
-          
-
-
-        </div>
+       
+        
 
     </>)
 }
